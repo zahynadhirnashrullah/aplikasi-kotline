72c7a260bda37b89c1a6f2ebaedbcd05ceb55b4d
zahy nadhir nashrullah <zahynadhir89@gmail.com>
2024-03-19 08:11:49 +0700

    Aquariumfish

---
+++ app/src/main/java/com/Aquariumfish.kt
@@ -0,0 +1,26 @@
+package com.example.appkotlin
+
+interface FishAction  {
+    fun eat()
+}
+interface FishColor {
+    val color: String
+}
+class Shark: FishAction, FishColor {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+object GoldColor : FishColor {
+    override val color = "gold"
+}
